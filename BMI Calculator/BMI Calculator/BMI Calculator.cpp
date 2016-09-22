4e2404977ebb5ba77a8d167838d9b19cba48e2e3
YSiyam <RgYazan62@gmail.com>
2016-09-22 16:43:20 -0400

    made it so it only asks once for height rather than twice, also made it cleaner and doubles went together and ints went together too

--- BMI Calculator/BMI Calculator/BMI Calculator.cpp
+++ BMI Calculator/BMI Calculator/BMI Calculator.cpp
@@ -1,46 +1,48 @@
 // BMI Calculator.cpp : Defines the entry point for the console application.
-//
+
 #include "stdafx.h"
 #include <iostream>
-/* This Program is meant to be a BMI calculator application
+
+
+/* This Program is meant to be a BMI calculator application 
 that reads the user’s weight in pound and height in feet and
 inches, then calculates and displays the users’ body mass index. */
 int main()
 {
 
-	//this is where i begin setup before going into the program
-	double weight; //this is weight in pounds
-	int heightf; //this is height in feet
-	int heighti;  //this is height in inches
-	int height; //this is the total height used in the bmi calculation
-	double bmi;  //this is the persons BMI
+	//this is where I begin setup before going into the program
 
-				 //the two commands bellow enable the use of decimals, without them cout won't display decimals
-	std::cout.precision(2);
-	std::cout.setf(std::ios::fixed);
+	double weight, bmi; //this is weight in pounds and their BMI
+	int heighti, heightf; //this is the total height used in the bmi calculation
+	
 
+	//the two commands bellow enable the use of decimals, without them cout won't display decimals
+
+	std::cout.precision(2); //the number in the bracket is how many decimals there will be in the number, this case 2
+	std::cout.setf(std::ios::fixed); 
+	
 	//this is where the calculations will happen for the variables initiated above
+
 	std::cout << "What is your weight in pounds? ";
 	std::cin >> weight;
 
-	std::cout << "What is your height in feet? ";
+	std::cout << "What is your height in feet and inches? ";
 	std::cin >> heightf;
-
-	std::cout << "What is your height in inches? ";
 	std::cin >> heighti;
 
-	height = (heightf * 12) + heighti;
-	bmi = (weight * 703) / (height * height);
-
+	heighti = (heightf * 12) + heighti;
+	bmi = (weight * 703) / (heighti * heighti);
+	
+	
 	//this section tells you what your BMI is and what you classify as
 
-	std::cout << "your BMI is: " << bmi << std::endl; //this tells you your bmi
+    std::cout << "your BMI is: " << bmi << std::endl; //this tells you your bmi
 
-													  /* these if statements only activate when
-													  a certain condition has been met which is declared in the brackets*/
+	/* these if statements only activate when
+	a certain condition has been met which is declared in the brackets*/
 
-	if (bmi >= 18.5 && bmi <= 24.9)  //this is the normal BMI statement
-	{
+	if ((bmi >= 18.5) && (bmi <= 24.9))  //this is the normal BMI statement
+	{ 
 		std::cout << "you have a normal BMI \n";
 	}
 
@@ -49,16 +51,17 @@ int main()
 		std::cout << "You are underweight \n";
 	}
 
-	if (bmi >= 25 && bmi <= 29.9) //this is the overweight BMI statement
+	if ((bmi >= 25) && (bmi <= 29.9)) //this is the overweight BMI statement
 	{
 		std::cout << "you are overweight! \n";
 	}
 
-	if (bmi >= 30) //this is the obese BMI statement
+	if (bmi >=30) //this is the obese BMI statement
 	{
 		std::cout << "you are obese! \n";
 	}
+
 	system("pause");
 
-	return 0;
+    return 0;
 }
