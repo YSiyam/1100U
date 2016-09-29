2bfadec57b4afed72ff92dbd9116a6619c8057d8
YSiyam <RgYazan62@gmail.com>
2016-09-29 16:20:49 -0400

    program is complete

--- salescontest/salescontest/salescontest.cpp
+++ salescontest/salescontest/salescontest.cpp
@@ -7,26 +7,33 @@
 //this application uses a while statement to determine and print the largest number of 10 positive numbers input by the user.
 int main()
 {
-	int counter = 0;
+
+	//this is where I initialize my variables
+	int counter = 1;
 	int number = 0;
 	int largest = 0;
 
-	while (counter < 10)
+	//this is the program itself
+	while (counter < 11) //this loop is to make sure that 10 numbers are put
 	{
-		while (number <= 0) 
+		while (number <= 0) //this makes sure that only numbers above 0 are put, if the user puts a number less than 0 the while loop repeats
 		{
-			std::cout << "enter any number greater than 0 \n";
+			std::cout << "enter a number greater than 0 \n";
 			std::cin >> number;
+			if (number <= 0) {  //this tells the user that negative numbers aren't allowed if he puts a number less than 0
+				std::cout << "negative numbers and 0 are not allowed \n";
+			}
 		}
 
-		if (largest <= number) 
+		if (largest <= number) //this puts the user inputted number in largest if it is larger than the preexisting number
 		{
 			largest = number;
 		}
-		number = 0;
-		counter++;
+		number = 0; //this resets number to 0 to re-execute the while loop after adding a counter
+		std::cout << "this was attempt number: " << counter << std::endl; //this tells the user what attempt he is at (max 10)
+		counter++; //this adds an "attempt" so the program doesn't go on an infinite loop
 	}
-	std::cout << "the largest number you entered is: " << largest << std::endl;
+	std::cout << " \n \n \n \n \n the largest number you entered is:   " << largest << std::endl; //this displays the largest number after the program is done
 	system("pause");
 
     return 0;
